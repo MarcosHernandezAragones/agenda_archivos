d3f574dec061791151bac638938e87e8b4c72eb3
Marcos Hernández Aragonés <marcoshernandezaragones@gmail.com>
2021-02-10 19:34:39 +0100

    acabado

--- src/Ejercicio/Main.java
+++ src/Ejercicio/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		IODatos.mostrar("alumnos.txt", 50);
+				IODatos.mostrar("alumnos.txt", 80);
 	}
 
 }
--- src/IO/IODatos.java
+++ src/IO/IODatos.java
@@ -10,75 +10,61 @@ import Ejercicio.Persona;
 
 public class IODatos {
 
-	public static void mostrar(String nombreFichero, int longitudLinea) {
+		public static void mostrar(String nombreFichero, int longitudLinea) {
+			File f = new File(nombreFichero);
+			FileReader fr = null;
+			Scanner leer = null;
+			if (!f.exists()) {
+				try {
+					f.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-		// ---------------------------------------------------------------------Crear
-		// archivo
-		File f = new File(nombreFichero);
+			}
 
-		FileReader fr = null;
-		Scanner leer = null;
-
-		if (!f.exists()) {
 			try {
-				f.createNewFile();
-			} catch (IOException e) {
+				fr = new FileReader(f);
+				leer = new Scanner(fr);
+				while (leer.hasNext()) {
+					String linea = leer.nextLine();
+					// tratamos las lineas
+					formatearLinea(linea,longitudLinea);
+				}
+			} catch (Exception e) { 
 				e.printStackTrace();
-			}
-		}
-		// ---------------------------------------------------------------------fin crear archivo
-		// ---------------------------------------------------------------------Leer
-		try {
-
-			fr = new FileReader(f);
-			leer = new Scanner(fr);
-			
-			
-			while (leer.hasNext()) {
-				String linea = leer.nextLine();
-				//Tratar las lineas
-				formatearLinea(linea, longitudLinea);
-			}
-		// ----------------------------------------------------------------------Fin leer
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-			
-		} finally {
-			
-			leer.close();
-			
-			try {
-				fr.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+			} finally {
+				try {
+					leer.close();
+					fr.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		
 
-	}
-
-	private static void formatearLinea(String linea,int longitudLinea) {
-		Persona p;
-		String nombre="", apellido="",aux;
-		int nip;
-		nip =Integer.parseInt(linea.substring(0, 6));
-		//sacar nombre y apellidos
-		aux=linea.substring(7);
 		
-		for (int i = 0; i < aux.length(); i++) {
+		
+
+		private static void formatearLinea(String linea, int longitudLinea) {
+			Persona p;
+			String nombre="", apellido="", aux;
+			int nip;
+			nip = Integer.parseInt(linea.substring(0, 6));
+			aux = linea.substring(7);
 			
-			//buscar la ,
-			if (aux.substring(i).equalsIgnoreCase(",")) {
-				apellido=aux.substring(8,i);
-				nombre=aux.substring(i+2);
+			for(int i=0;i<aux.length();i++) {
+				if(aux.substring(i,i+1).equals(",")) {
+					apellido=aux.substring(0,i);
+					nombre = aux.substring(i+2);
+					
+				}
 			}
-			
+			p = new Persona(nombre, apellido, nip);
+			System.out.println(p.mostrar(longitudLinea));
 			
 		}
-		
-		p= new Persona(nombre,apellido,nip);
-		System.out.println(p);
-	}
 
 }
